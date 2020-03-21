767556b2b726550bdce64cd7d70273bba269d4c7
NaimaZulfa <naima.zulfa@students.amikom.ac.id>
2020-03-21 21:44:38 +0700

    inisialisasi project

---
+++ LatClassTaxi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LatClassTaxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.driverName = "Zulfa";
+            taxi.onDuty = true;
+            taxi.NumPassenger = 10;
+
+
+            taxi.TaxiInfo();
+
+
+            Console.WriteLine("\n");
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LatClassTaxi/Taxi.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LatClassTaxi
+{
+    class Taxi
+    {
+        public string driverName { get; set; }
+        public bool onDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", driverName);
+            if (onDuty)
+            {
+                Console.WriteLine("On Duty : Yes");
+            }
+            else
+            {
+                Console.WriteLine("On Duty : No");
+            }
+
+            Console.WriteLine("Number of Passenger : {0}", NumPassenger);
+
+        }
+
+        public void PickUpPassenger()
+        {
+            if (onDuty)
+            {
+                Console.WriteLine("{0} sedang menjemput penumpang", driverName);
+            }
+            else
+            {
+                Console.WriteLine("{0} tidak menjemput penumpang", driverName);
+            }
+        }
+
+        public void DropOffPassenger()
+        {
+            if (onDuty)
+            {
+                Console.WriteLine("{0} selesai mengantar penumpang", driverName);
+            }
+            else
+            {
+                Console.WriteLine("{0} tidak mengantar penumpang", driverName);
+            }
+        }
+    }
+}
